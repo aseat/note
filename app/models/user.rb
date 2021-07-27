fb4f1c3e73c6288f9650abe93ca8b239e3e4fe85
aseat <lemnlsdrye231mk@gmail.com>
2021-07-27 14:26:39 +0900

    ユーザ管理機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+         with_options presence: true do
+          validates :creator
+          validates :noteid, format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/, message: 'Include both letters and numbers' }
+          validates :encrypted_password, :password, length: { minimum: 8 }, format: { with: /\A[\w@-]*[A-Za-z][:-@¥[-`{-~]+$]*\z/ , message: 'Include both letters and numbers' }
+        end
 end
