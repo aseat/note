c387a7f46e7289f5f048d6541afea738cd9f98ad
aseat <lemnlsdrye231mk@gmail.com>
2021-07-27 12:44:58 +0900

    initial

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    @user.password_confirmation = user_params[:password]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:alert] = @user.errors.full_messages
+      redirect_to root_path
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:creator, :noteid, :email, :password, :password_confirmation)
+    end
+end
