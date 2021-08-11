15844b2a2550460952062118dcac3f9778b288a3
aseat <lemnlsdrye231mk@gmail.com>
2021-08-11 21:59:52 +0900

    マークダウン記述

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_131915) do
+ActiveRecord::Schema.define(version: 2021_08_11_101648) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
