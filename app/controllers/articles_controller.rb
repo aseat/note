b03a94c47cbf2746f7b953144ae353b00e652086
aseat <lemnlsdrye231mk@gmail.com>
2021-09-13 15:30:27 +0900

    ユニコーン

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[show edit update destroy]
+  before_action :set_article, only: %i[show edit update destroy pay]
   before_action :authenticate_user!, except: [:index, :show]
+  protect_from_forgery except: :pay
+  require "payjp"
+
   # GET /articles or /articles.json
   def index
     @article = Article.includes(:user).order('created_at DESC')
@@ -8,11 +11,19 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+
   end
 
   # GET /articles/new
   def new
     @article = Article.new
+   @article.tags.build
+   @article.article_bodies.build
+    if request.xhr?
+      respond_to do |format|
+        format.js
+      end
+    end
   end
 
   # GET /articles/1/edit
@@ -23,7 +34,6 @@ class ArticlesController < ApplicationController
   # POST /articles or /articles.json
   def create
     @article = Article.new(article_params)
-
     respond_to do |format|
       if @article.save
         format.html { redirect_to root_path, notice: 'Article was successfully created.' }
@@ -32,6 +42,19 @@ class ArticlesController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  def pay
+    Payjp.api_key = 'sk_test_71c136029357931ccc79ff72'
+    Payjp::Charge.create(
+      amount: 3700,
+      card: params['payjp-token'],
+      currency: 'jpy'
+    )
+    if @article.update(buyer_id: current_user.id)
+      redirect_to controller: "articles", action: 'show'
+    else
     end
   end
 
@@ -70,10 +93,12 @@ class ArticlesController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_article
     @article = Article.find(params[:id])
-  end
+    @article_bodies=ArticleBody.find(params[:id])
+    end
 
   # Only allow a list of trusted parameters through.
   def article_params
-    params.require(:article).permit(:title, :body, :image,:tag).merge(user_id: current_user.id)
+    params.require(:article).permit(:title,:image,:buyer_id,:item_id, tags_attributes: [:id, :text, :_destroy], article_bodies_attributes: [:id,  :body,{images: []},:_destroy]).merge(user_id: current_user.id)
   end
 end
+0----00-
