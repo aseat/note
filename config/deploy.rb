915e2a473b9ffc6c4b483006b1941cb5329bce0a
aseat <lemnlsdrye231mk@gmail.com>
2021-09-17 15:13:46 +0900

    デプロイ

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.16.0"
 
 set :application, "note"
-set :repo_url, "https://github.com/aseat/note.git"
+set :repo_url, "git@github.com:aseat/note.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
